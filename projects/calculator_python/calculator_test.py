cd84aa086f2d7d5c3a8d6a6555a625c088e2ea0f
Mauro9735 <mauriciovilladiego1999@gmail.com>
2023-04-27 13:29:03 -0500

    Adding files to practice bazel

---
+++ projects/calculator_python/calculator_test.py
@@ -0,0 +1,12 @@
+import unittest
+from projects.calculator_python.calculator import Calculator
+
+class TestSum(unittest.TestCase):
+    def test_sum(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.add(2,1),3)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
